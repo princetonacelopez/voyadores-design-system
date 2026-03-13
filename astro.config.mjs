bd9f1cb6138115cf5b4cc89cf8b3e3596a1a2212
Princeton Ace Lopez <plopez@nesventures.com>
2026-03-13 12:31:14 +0800

    fix based domain issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://voyadores-design-system.vercel.app',
   vite: {
     css: {
       devSourcemap: true,
